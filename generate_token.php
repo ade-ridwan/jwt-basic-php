daef3a3c3a12ff082a3b1c25713cfe5bb17bd042
Ade Ridwan <49741216+Readone45@users.noreply.github.com>
2020-05-01 05:02:13 +0700

    Update generate_token.php

--- generate_token.php
+++ generate_token.php
@@ -7,7 +7,7 @@ $jwthelper = new JwtHelper;
 //insialisasi batas waktu token
 $issuedAt   = time(); //waktu token dibuat
 $notBefore  = $issuedAt + 10;  // token akan valid setelah 10 second di generate.
-$expire     = $notBefore + 60; // 60 detik merupakan waktu masa berlaku nya token dalam satuan.
+$expire     = $notBefore + 60; // 60 detik merupakan waktu masa berlaku nya token.
 
 //data yang akan dikirim melalui header 
 //bisa di sesuaikan dengan data yang sobat butuhkan.
