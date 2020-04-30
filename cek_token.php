5482b2b379f09e9248861946a2b2580515f43d42
triannoviandi <triannoviandi00@gmail.com>
2020-05-01 05:05:11 +0800

    add expired feature

--- cek_token.php
+++ cek_token.php
@@ -1,5 +1,6 @@
 <?php
 require('helper/jwt_helper.php');
+include 'account.php';
 
 //Inisialisasi class JwtHelper
 $jwthelper = new JwtHelper;
@@ -8,15 +9,26 @@ $jwthelper = new JwtHelper;
 header('Content-Type: application/json');
 
 // Cek jika header request berisi Authorization
-if(isset(apache_request_headers()["Authorization"])){
+if (isset(apache_request_headers()["Authorization"])) {
 	$headers = apache_request_headers();
 	$jwt = $headers['Authorization'];
 
 	$payload = $jwthelper->decoded_jwt($jwt);
-	echo json_encode($payload);
-	
-}else{
-// Jika tidak ada maka munculkan pesan berikut
+	$expire_date = DateTime::createFromFormat('Y-m-d', $payload->expire_date)->format('Y-m-d');
+
+	// Mengecek jika tanggal sekarang masih kurang dari tanggal expire token
+	if (date('Y-m-d') <= $expire_date) {
+		echo json_encode($payload);
+	} else {
+		http_response_code(401);
+		$error = [
+			"message" => "Token Expired",
+			"code" => http_response_code()
+		];
+		echo json_encode($error);
+	}
+} else {
+	// Jika tidak ada maka munculkan pesan berikut
 	http_response_code(401);
 	$error = [
 		"message" => "Authorization Requested !",
@@ -24,4 +36,3 @@ if(isset(apache_request_headers()["Authorization"])){
 	];
 	echo json_encode($error);
 }
-	
--- generate_token.php
+++ generate_token.php
@@ -1,16 +1,41 @@
 <?php
 require('helper/jwt_helper.php');
+include 'account.php';
 
 //menginisialisasi object class JwtHelper
 $jwthelper = new JwtHelper;
 
-//data yang akan dikirim melalui header 
-//bisa di sesuaikan dengan data yang sobat butuhkan.
-$payload = [
-"id_user" => "1",
-"email" => "sukun024@gmail.com"
-];
+// Mendapatkan dan menginisialisasi body yang dikirimkan
+$body = json_decode(file_get_contents("php://input"));
 
-//menampilkan token yg sudah dibuat;
-echo $jwthelper->generate_jwt($payload);
-?>
+// Cek jika body berisikan email dan password yang sama saat token di generate.
+if ($body->email == $email and $body->password == $password) {
+
+    // Jika berhasil, kemudian membuat payload yang digunakan untuk menghasilkan 
+    // token. terdiri dari email, password, dan expire_date untuk menentukan masa
+    // berlaku token yakni 3 hari.
+
+    $expire_date = date('Y-m-d', strtotime(' + 3 days'));
+    $payload = [
+        'email' => $email,
+        'password' => $password,
+        'expire_date' => $expire_date
+    ];
+
+    //menampilkan token yg sudah dibuat;    
+    http_response_code(200);
+    $success = [
+        "message" => "Success",
+        "code" => http_response_code(),
+        "token" => $jwthelper->generate_jwt($payload)
+    ];
+    echo json_encode($success);
+} else {
+    // Jika tidak ada maka munculkan pesan berikut
+    http_response_code(401);
+    $error = [
+        "message" => "Wrong email & password !",
+        "code" => http_response_code()
+    ];
+    echo json_encode($error);
+}
