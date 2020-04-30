8f4969f195dcade3412c7c5ab21b3942a50623a3
Ade Ridwan <49741216+Readone45@users.noreply.github.com>
2020-05-01 02:38:25 +0700

    Menambah keterangan script

--- helper/jwt_helper.php
+++ helper/jwt_helper.php
@@ -3,20 +3,25 @@ require('vendor/autoload.php');
 use \Firebase\JWT\JWT;
 
 class JwtHelper {
+	//key ini bukan patokan dan kalian bebas menggantinya
 	public 	$key = "0753370551417f2d7b72acb6c260d39d83741d36fd4ab01e4f82a3235e43aa63";
-
+	
+	//function untuk membuat token
 	public function generate_jwt($payload){	
 		$jwt = JWT::encode($payload, $this->key);
 		return $jwt;
 	}
-
+	
+	//function untuk menerjemahkan token dan mengambil data yg kita kirim pada payload
 	public function decoded_jwt($jwt){
 		try{
+			//jika token sesuai akan mengembalikan pesan berikut
 			$decoded = JWT::decode($jwt, $this->key, array('HS256'));
 			$decoded->code = http_response_code();
 			return $decoded;
 		}
 		catch (Exception $ex){
+			//jika token tidak sesuai makan mengembalikan pesan berikut
 			http_response_code(401);
 			$decoded = [
 				"message" => "Unauthroizhed",
