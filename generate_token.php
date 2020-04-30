03dc63ff7f93162d141977ee4682be908ab5bb92
Ade Ridwan <49741216+Readone45@users.noreply.github.com>
2020-05-01 03:03:58 +0700

    Update generate_token.php

--- generate_token.php
+++ generate_token.php
@@ -4,7 +4,7 @@ require('helper/jwt_helper.php');
 //menginisialisasi object class JwtHelper
 $jwthelper = new JwtHelper;
 
-//data yang akan dikirim melalui header
+//data yang akan dikirim melalui header bisa di sesuaikan dengan data yang sobat butuhkan.
 $payload = [
 "id_user" => "1",
 "email" => "sukun024@gmail.com"
